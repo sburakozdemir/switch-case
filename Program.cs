04a773f62ca1bc9eba91f3ad8504150c9b302392
sburakozdemir <sburakozdemir00@gmail.com>
2022-12-29 00:57:40 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int month = DateTime.Now.Month;
+switch (month)
+{
+    case 1 :
+    Console.WriteLine("Ocak Ayındasınız");
+    break;
+     case 2 :
+    Console.WriteLine("Şubat Ayındasınız");
+    break;
+     case 12 :
+    Console.WriteLine("Aralık Ayındasınız");
+    break;
+
+
+    default:
+    Console.WriteLine("Yanlış Veri Girişi");
+    break;
+}
+
+switch (month)
+{
+     case 12:
+     case 1:
+     case 2:
+        Console.WriteLine("Kış Ayındasınız");
+        break;
+
+    default:
+    break;
+}
